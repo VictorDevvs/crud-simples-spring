8f6854e0610725e3239bab003904654158bc01d9
VictorDevvs <victorsantosdevvs@gmail.com>
2025-05-21 09:55:55 -0300

    criando o método get para buscar um user pelo id

--- crudsimples/src/main/java/com/victordevvs/crudsimples/controller/UserController.java
+++ crudsimples/src/main/java/com/victordevvs/crudsimples/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.victordevvs.crudsimples.controller;
 
+import com.victordevvs.crudsimples.dto.UserResponseDto;
+import com.victordevvs.crudsimples.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserResponseDto findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
 }
