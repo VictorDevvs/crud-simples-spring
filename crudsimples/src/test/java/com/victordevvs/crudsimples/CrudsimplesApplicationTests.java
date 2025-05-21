0d280d6f4f866a4b09f9411b021cd27524d20d97
VictorDevvs <victorsantosdevvs@gmail.com>
2025-05-21 08:57:14 -0300

    projeto criado

---
+++ crudsimples/src/test/java/com/victordevvs/crudsimples/CrudsimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victordevvs.crudsimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
