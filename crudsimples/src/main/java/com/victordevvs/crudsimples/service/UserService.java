16c02804a897912d41d754a1f56c50e669cedbb6
VictorDevvs <victorsantosdevvs@gmail.com>
2025-05-21 09:24:01 -0300

    criando o UserService juntamente com o método save

---
+++ crudsimples/src/main/java/com/victordevvs/crudsimples/service/UserService.java
@@ -0,0 +1,37 @@
+package com.victordevvs.crudsimples.service;
+
+import com.victordevvs.crudsimples.dto.UserRequestDto;
+import com.victordevvs.crudsimples.dto.UserResponseDto;
+import com.victordevvs.crudsimples.mapper.UserMapper;
+import com.victordevvs.crudsimples.model.User;
+import com.victordevvs.crudsimples.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.security.KeyStore;
+
+@Slf4j
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Transactional
+    public UserResponseDto save(UserRequestDto userRequestDto) {
+        log.info("Received request to save user");
+        userRepository.findByEmail(userRequestDto.getEmail()).ifPresent(u -> {
+            //throw new ExistingEmailException("The email is already in use");
+        });
+        User user = userMapper.toModel(userRequestDto);
+        User savedUser = userRepository.save(user);
+        log.info("User saved with id: {}", savedUser.getId());
+        return userMapper.toDto(savedUser);
+    }
+}
